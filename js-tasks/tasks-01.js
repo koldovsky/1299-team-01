1fe3f49f84ed747e397c9d2bf160db89ee1fda3d
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2024-11-15 19:29:45 +0200

    Create tasks-01.js

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,31 @@
+// https://www.codewars.com/kata/57a429e253ba3381850000fb/train/javascript
+
+function bmi(weight, height) {
+  const bmi = weight / height ** 2;
+  if (bmi <= 18.5) {
+    return "Underweight";
+  }
+  if (bmi <= 25) {
+    return "Normal";
+  }
+  if (bmi <= 30) {
+    return "Overweight";
+  }
+  return "Obese";
+}
+
+// https://www.codewars.com/kata/57a429e253ba3381850000fb/train/javascript
+
+function bmi(weight, height) {
+  const bmi = weight / height ** 2;
+  if (bmi <= 18.5) {
+    return "Underweight";
+  }
+  if (bmi <= 25) {
+    return "Normal";
+  }
+  if (bmi <= 30) {
+    return "Overweight";
+  }
+  return "Obese";
+}
