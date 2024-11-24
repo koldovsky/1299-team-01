f09bf3cbb74e280085590246d72db578c0204469
sherlock440 <soroka2179@gmail.com>
2024-11-24 16:11:51 +0200

    Create and attach JS

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 function init() {
-    import('index.key-speakers-members-carousel.js');
+  import("index.key-speakers-members-carousel.js");
 }
 
-const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+const totalPartials = document.querySelectorAll(
+  '[hx-trigger="load"], [data-hx-trigger="load"]'
+).length;
 let loadedPartialsCount = 0;
 
-document.body.addEventListener('htmx:afterOnLoad', () => {
-    loadedPartialsCount++;
-    if (loadedPartialsCount === totalPartials) init();
+document.body.addEventListener("htmx:afterOnLoad", () => {
+  loadedPartialsCount++;
+  if (loadedPartialsCount === totalPartials) init();
 });
