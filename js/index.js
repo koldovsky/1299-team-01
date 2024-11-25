ce21ce3fd4791a0a94a33737eba9b41b8281ef49
AnnaPannaCotta <myrzilka.anya@gmail.com>
2024-11-25 15:57:21 +0200

    add js for ticket partial

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function init() {
   import("./index.key-speakers-members-carousel.js");
   import("./index.key-speakers-members-carousel-clippy.js");
   import("./index.key-speakers-members.js");
+  import('./index.ticket-prices-carousel.js')
 }
 
 const totalPartials = document.querySelectorAll(
