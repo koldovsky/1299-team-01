275cfe2bee71ee31ff02cc91d5efa478092879d5
sherlock440 <soroka2179@gmail.com>
2024-11-29 19:41:48 +0200

    My partials are supposed to be ready

--- Soroka-Particial/js/index.header-hero.js
+++ Soroka-Particial/js/index.header-hero.js
@@ -1,4 +1,5 @@
-// GitHub Pages broke the fuck up my relative paths so my code used to work on my computer but used to not work on GitHub Pages. So I have to dynamically find a root folder in order to use absolute paths
+// GitHub Pages breaks the fuck up my relative paths so my code used to work on my computer but used to not work on GitHub Pages.
+// So I have to dynamically find a root folder in order to use absolute paths
 
 const repoName = "1299-team-01"; // our folder name on GitHub Pages
 const isGitHubPages = window.location.pathname.includes(`/${repoName}/`); // TRUE if the folder exists in the location path (when I run it on my computer there is no this folder on the URL)
@@ -18,20 +19,22 @@ function goWildRR() {
   const audio = document.getElementById("rr");
   const element = document.querySelector(".hero");
 
-  const replacingBackground = `url(${replacingImage})`;  // a variable for storing value for CSS's 'background-image' property
-  const originalBackground = `url(${originalImage})`;  // a variable for storing value for CSS's 'background-image' property
+  const replacingBackground = `url(${replacingImage})`; // a variable for storing value for CSS's 'background-image' property
+  const originalBackground = `url(${originalImage})`; // a variable for storing value for CSS's 'background-image' property
 
-  button.addEventListener("click", () => {  // after clicking
-    element.style.backgroundImage = replacingBackground;  // replaces 'backgroung-image' in CSS
+  button.addEventListener("click", () => {
+    // after clicking
+    element.style.backgroundImage = replacingBackground; // replaces 'backgroung-image' in CSS
 
-    button.classList.add("active");  // add class 'active' to the button to use CSS styles fo active-element (button after clicking) as non-transparency
+    button.classList.add("active"); // add class 'active' to the button to use CSS styles fo active-element (button after clicking) as non-transparency
 
     audio.play();
 
-    audio.addEventListener("ended", () => {  // when audio is ended
-      element.style.backgroundImage = originalBackground;  // replaces new 'backgroung-image' with the previous one in CSS
+    audio.addEventListener("ended", () => {
+      // when audio is ended
+      element.style.backgroundImage = originalBackground; // replaces new 'backgroung-image' with the previous one in CSS
 
-      button.classList.remove("active");  // deleting newly created active class so that its styles as non-transparant get deleted
+      button.classList.remove("active"); // deleting newly created active class so that its styles as non-transparant get deleted
     });
   });
 }
