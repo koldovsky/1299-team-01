0c06a0c53f5a9e0534d8f02d8dad8f06ca2675d5
unknown <an.vasilyeva99@gmail.com>
2024-12-01 17:32:46 +0200

    add id for partials,add js for contact form

---
+++ js/index.contact-form.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("contact-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  
+  const submitButton = event.submitter; // Отримуємо кнопку, яка ініціювала подію
+  if (submitButton && submitButton.textContent.trim() === "Leave a request") {
+    const response = await fetch(form.action, {
+      method: form.method,
+      body: new FormData(form),
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    const status = document.getElementById("contact-form-status");
+    if (response.ok) {
+      status.innerHTML = "Thanks for your submission!";
+      form.reset();
+    } else {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+    }
+  }
+}
+
+form.addEventListener("submit", handleSubmit);
